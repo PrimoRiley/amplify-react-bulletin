bc92d2b87571b1950a9f7b1f4362dbb80333716f
Riley Primeau <primeauriley@gmail.com>
2023-12-03 18:27:55 -0700

    authorization

---
+++ bulletin_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactbulletindc0d25f9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
